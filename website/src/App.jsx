8556bb3c35207170e054833043950b1a6cf52ca7
swstk <swstk@swstks-MacBook-Air.local>
2023-09-08 18:45:33 +0530

    k

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import ImageGrid from './components/sidebar'
+import SpeechRecognizer from './components/SpeechRecognizer'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -9,10 +10,11 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-      <>
-    <ImageGrid/>
-    <Layout/>
-      </>
+    <>
+      <ImageGrid />
+      <Layout />
+      <SpeechRecognition />
+    </>
   )
 }
 
---
+++ website/src/components/SpeechRecognizer.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import SpeechRecognition, { useSpeechRecognition } from "react-speech-recognition";
+
+const SpeechRecognizer = () => {
+    const {
+        transcript,
+        listening,
+        resetTranscript,
+        browserSupportsSpeechRecognition
+    } = useSpeechRecognition();
+
+    if (!browserSupportsSpeechRecognition)
+        return <span>Your Browser does not support Speech to Text recognition!</span>;
+
+    return (
+        <div>
+            <p>Microphone: {listening ? 'on' : 'off'}</p>
+            <button onClick={SpeechRecognition.startListening}>Start</button>
+            <button onClick={SpeechRecognition.stopListening}>Stop</button>
+            <button onClick={resetTranscript}>Reset</button>
+            <p>{transcript}</p>
+        </div>
+    );
+}
+
+export default SpeechRecognizer;
