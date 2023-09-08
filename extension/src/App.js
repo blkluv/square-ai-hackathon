a777f72e7cbd379897d32879b9175bc8c4995ab4
Yuvraj Singh <ec21b1011@iiitdm.ac.in>
2023-09-08 16:23:47 +0530

    added extension

---
+++ extension/src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="welcome">
+        <h1>This is square up extension</h1>
+        <p>Created using React</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
