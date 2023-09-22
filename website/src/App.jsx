7b92628ad88c47be807bc78e5a382f82cf5ada76
Yuvraj Singh <ec21b1011@iiitdm.ac.in>
2023-09-23 03:50:51 +0530

    removed import in App.jsx

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -6,7 +6,6 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Layout from './components/layout'
-import StraightArrow from './components/StraightArrow';
 
 function App() {
   const [count, setCount] = useState(0)
