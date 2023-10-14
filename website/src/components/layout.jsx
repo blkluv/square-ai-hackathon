910208b1602dd34b92740b4131fd200cc9d9308b
jashwanth0712 <jashwanth0712@gmail.com>
2023-10-14 19:53:02 +0530

    updated map path finder algo

--- website/src/components/layout.jsx
+++ website/src/components/layout.jsx
@@ -12,8 +12,6 @@ let destCol = 0;
 const cellSize = 40;
 
 function calculateCenterPosition(row, col) {
-  const [source,setsource]=useState(null)
-  const [destination,setdestination]=useState(null);
   // Calculate the center position of the cell
   const x = (col + 1) * cellSize + cellSize / 2 - 20;
   const y = (row + 1) * cellSize + cellSize / 2 - 20;
@@ -21,6 +19,9 @@ function calculateCenterPosition(row, col) {
 }
 
 const Layout = () => {
+  const [source, setSource] = useState({ row: -1, col: -1 });
+  const [destination, setDestination] = useState({ row: -1, col: -1 });
+  const [arrow, setArrow] = useState(null);
   const gridState = [
     [1, 1, 1, 0, 1, 1, 1],
     [1, 0, 0, 0, 1, 0, 1],
@@ -30,111 +31,83 @@ const Layout = () => {
     [1, 1, 1, 0, 0, 0, 1],
     [1, 1, 0, 0, 1, 1, 1],
   ];
-  const stops = [
-    [1, 1],
-    [1, 2],
-    [1, 3],
-    [2, 3],
-    [4, 3],
-    [5, 3],
-    [5, 4],
-    [5, 5],
-    [1, 5],
-  ];
-  const [source, setSource] = useState({ row: -1, col: -1 });
-  const [destination, setDestination] = useState({ row: -1, col: -1 });
+  useEffect(() => {
+    // Whenever source or destination changes, redraw the arrow
+    const arrow = drawArrow([
+      [source.row, source.col],
+      [destination.row, destination.col],
+    ]);
 
-  let counter = 0;
+    // Update the state with the new arrow
+    setArrow(arrow);
+  }, [source, destination]);
+  // const stops = [
+  //   [1, 1],
+  //   [1, 2],
+  //   [1, 3],
+  //   [2, 3],
+  //   [4, 3],
+  //   [5, 3],
+  //   [5, 4],
+  //   [5, 5],
+  //   [1, 5],
+  // ];
+    let counter = 0;
   const updatesource=()=>{
-    setSource([1,1])
+    setSource({ row: 1, col: 1 })
     console.log("source updated")
   };
   const updatedestination=()=>{
-    setDestination([2,3])
+    setDestination({ row: 1, col: 4 })
   }
+  
   const drawArrow = (stops) => {
     counter += 1;
     console.log("counter ", counter);
     let arrowHTML = [];
-    for (let i = 0; i < stops.length - 1; i++) {
-      let tempSource = stops[i];
-      const tempDest = stops[i + 1];
-      const tempPath = aStar(gridState, tempSource, tempDest);
-      // console.log(i, " ", tempPath)
 
-      for (let j = 0; j < tempPath.length; j++) {
-        // console.log('i = ', i, 'j = ', j)
-        let dest = [
-          tempSource[0] + tempPath[j][0],
-          tempSource[1] + tempPath[j][1],
-        ];
-        source.row = tempSource[0];
-        source.col = tempSource[1];
-        destination.row = dest[0];
-        destination.col = dest[1];
-        // console.log(source.row,source.col,destination.row,destination.col)
-        const sourceCenter = calculateCenterPosition(source.row, source.col);
-        const destinationCenter = calculateCenterPosition(
-          destination.row,
-          destination.col
-        );
-        if (source.row !== -1 && destination.row !== -1) {
+    // Only draw the arrow if both source and destination are set
+    
+    if (source.row !== -1 && destination.row !== -1) {
+      for (let i = 0; i < 1; i++) {
+        let tempSource = stops[i];
+        const tempDest = stops[stops.length - 1];
+        const tempPath = aStar(gridState, tempSource, tempDest);
+
+        for (let j = 0; j < tempPath.length; j++) {
+          let dest = [
+            tempSource[0] + tempPath[j][0],
+            tempSource[1] + tempPath[j][1],
+          ];
+
+          const sourceCenter = calculateCenterPosition(tempSource[0], tempSource[1]);
+          const destinationCenter = calculateCenterPosition(dest[0], dest[1]);
+
           const dx = destinationCenter.x - sourceCenter.x;
           const dy = destinationCenter.y - sourceCenter.y;
           const angle = Math.atan2(dy, dx) * (180 / Math.PI);
 
           arrowHTML.push(
             <div
-            style={{
-              className:"animate-bounce",
-              top: sourceCenter.y,
-              left: sourceCenter.x,
-              width: Math.sqrt(dx * dx + dy * dy),
-              transform: `rotate(${angle}deg)`,
-              transformOrigin: "left center",
-              borderRadius: "5px",
-              borderBottom: "7px dashed green", // Arrow styling as a dashed line
-              position: "absolute",
-              animation: "moveDots 2s linear infinite", // Add animation
-            }}
-          ></div>
+              key={`arrow-${i}-${j}`}
+              style={{
+                top: sourceCenter.y,
+                left: sourceCenter.x,
+                width: Math.sqrt(dx * dx + dy * dy),
+                transform: `rotate(${angle}deg)`,
+                transformOrigin: "left center",
+                borderRadius: "5px",
+                borderBottom: "7px dashed green",
+                position: "absolute",
+                animation: "moveDots 2s linear infinite",
+              }}
+            ></div>
           );
-          // arrowHTML.push(
-          //   <div
-          //     style={{
-          //       top: destinationCenter.y,
-          //       left: destinationCenter.x,
-          //       width: 15,
-          //       transform: `rotate(${angle + 160}deg)`,
-          //       transformOrigin: "left center",
-          //       borderBottom: "2px solid red", // Arrow styling
-          //       position: "absolute",
-          //     }}
-          //   >
-              
-          //   </div>
-          // );
-          // arrowHTML.push(
-          //   <div
-          //     style={{
-          //       top: destinationCenter.y,
-          //       left: destinationCenter.x,
-          //       width: 15,
-          //       transform: `rotate(${angle + 200}deg)`,
-          //       transformOrigin: "left center",
-          //       borderBottom: "2px solid red", // Arrow styling
-          //       position: "absolute",
-          //     }}
-          //   >   
-            // </div>
-          // );
 
           tempSource = [...dest];
         }
       }
     }
-    // console.log("outputHtml ", arrowHTML)
-    // return outputHtml;
     return arrowHTML;
   };
   const grid = [
@@ -179,19 +152,17 @@ const Layout = () => {
         
         <div className="&larr"></div>
 
-         {drawArrow([
-    [1, 1],
-    [2, 3],
-    [5, 3],
-    [5, 5],
-    [1, 5],
-  ])} 
+         {source!={ row: -1, col: -1 }  && destination!={ row: -1, col: -1 } && drawArrow([
+          [source.row,source.col],
+
+          [destination.row,destination.col],
+        ])} 
       </div>
       <div >
-        {"source "+source[0]+" , "+source[1]}
+        {"source "+source.row+" , "+source.col}
       </div>
       <div>
-      {"destination "+destination[0]+" , "+destination[1]}
+      {"destination "+destination.row+" , "+destination.col}
 
       </div>
     <button onClick={updatesource}>update source</button>
