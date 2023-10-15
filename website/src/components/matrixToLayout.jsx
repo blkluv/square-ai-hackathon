9c31659d52d90a69edad8a1b71543bd49de569b1
Ronnieraj37 <101713157+Ronnieraj37@users.noreply.github.com>
2023-10-15 12:21:00 +0530

    Updated drag

--- website/src/components/matrixToLayout.jsx
+++ website/src/components/matrixToLayout.jsx
@@ -1,23 +1,62 @@
+/* eslint-disable react/prop-types */
+import {useEffect, useState} from "react"
 import itemList from "../constants/itemImages";
 import itemNames from "../constants/itemNames";
+import { useDrag, useDrop } from 'react-dnd';
+import { DndProvider } from 'react-dnd';
+import {HTML5Backend} from 'react-dnd-html5-backend';
+const MatrixElement = ({ value, rowIndex, colIndex, onDrop }) => {
+  const [, ref] = useDrag({
+    type: 'MATRIX_ELEMENT',
+    item: { rowIndex, colIndex },
+  });
+
+  const [, drop] = useDrop({
+    accept: 'MATRIX_ELEMENT',
+    drop: (item) => {
+      // Handle the drop action here, and then call the onDrop callback.
+      onDrop(item, { rowIndex, colIndex });
+    },
+  });
+
+  return (
+    <div ref={(node) => ref(drop(node))} className="matrix-element">
+      {/* {value} */}
+      <img src={itemList[value]} alt="" className="w-full h-full m-0" />
+    </div>
+  );
+};
 const MatrixToLayout=(props)=>{
-    const matrix=props.matrix.matrix
+    const [matrix, setmatrix] = useState(props.matrix.matrix);
     console.log(matrix)
+    const handleDrop = (item, target) => {
+      // Implement your logic for handling the drop action here,
+      // which might involve swapping matrix elements.
+      const before = matrix[item.rowIndex][item.colIndex];
+      const after = matrix[target.rowIndex][target.colIndex] ;
+      const _matrix = matrix;
+      _matrix[item.rowIndex][item.colIndex] =  after;
+      _matrix[target.rowIndex][target.colIndex] = before;
+      setmatrix(_matrix);
+      console.log("HEre",item,target);
+    };
+    useEffect(()=>{console.log("matrix updated")},[matrix])
     return(
-        <div
+        <DndProvider backend={HTML5Backend}
         className="grid grid-cols-100 gap-0 p-5 bg-white rounded-lg w-fit"
         style={{ gridTemplateColumns: `repeat(${matrix.length}, 1fr)` }}>
         {matrix.map((row, rowIndex) =>
           row.map((image, colIndex) => (
-            <div
-              key={`${rowIndex}-${colIndex}`}
-            >
-            <img src={itemList[image]} alt="" className="w-full h-full m-0" />
-
-            </div>
-          ))
+              <MatrixElement
+              key={colIndex}
+              value={image}
+              rowIndex={rowIndex}
+              colIndex={colIndex}
+              onDrop={handleDrop}
+            />
+            ))
         )}
-        </div>
+        </DndProvider>
     )
 }
 export default MatrixToLayout;
