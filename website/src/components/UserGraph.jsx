81609c5b300ab6b191b3e734a1935df4a2496265
jashwanth0712 <jashwanth0712@gmail.com>
2023-10-18 11:57:05 +0530

    Modify UserGraphs

--- website/src/components/UserGraph.jsx
+++ website/src/components/UserGraph.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import Graph from "react-vis-graph-wrapper";
-
+import { Clusters, tags, tagNames } from "../constants/clusters"
+import Confetti from 'react-confetti'
+import useWindowSize from 'react-use/lib/useWindowSize'
 // import "./styles.css";
 // // need to import the vis network css in order to show tooltip
 // import "./network.css";
@@ -11,7 +13,7 @@ function getUserGraph(matrix) {
     };
     const n = matrix.length;
     for (let i = 0; i < n; i++) {
-        graph.nodes.push({ id: i, label: "Node"+i });
+        graph.nodes.push({ id: i, label: ""+i });
         for (let j = 0; j < n; j++) {
         if (matrix[i][j] === 1) {
             graph.edges.push({ from: i, to: j });
@@ -21,6 +23,7 @@ function getUserGraph(matrix) {
     return graph;
 }
 function UserGraph() {
+  
     const handleNodeClick = ((node) => {
         if (selectedNodes.includes(node)) {
             // If the button is already selected, remove it
@@ -86,8 +89,21 @@ const graph = getUserGraph(hardcoded_list)
         handleNodeClick(node);
       },
   };
+  const done =()=>{
+    settem(true)
+  }
+  const { width, height } = useWindowSize()
+  
+  const [tem,settem]=useState(false)
   return (
     <div className="mt-20">
+      {tem&&
+        <Confetti
+        width={width}
+        height={height}
+        recycle={false} 
+      />
+      }
       <div className="left-half">
         <Graph
           graph={graph}
@@ -100,10 +116,27 @@ const graph = getUserGraph(hardcoded_list)
             
       </div>
       <div className="right-half">
-        <h1>Selected Nodes</h1>
+        <h2 className="text-4xl ml-2">Selected Nodes</h2>
+        {/* <h2>Tags</h2>
+      <pre>{JSON.stringify(tags, null, 2)}</pre> */}
         <ul>
           {selectedNodes.map((item, index) => (
-            <li className="text-white" key={index}>{item}</li>
+            <>
+            <div className="flex">
+              <h2 className="mt-10 mb-5 mr-5">Group {item}</h2>
+              <button style={{height:"fit-content ",width:"fit-content"}} onClick={done}>Send custom gift card</button>
+            </div>
+            <ul className="text-white flex">
+            {tags[item].map((i, index) => (
+              <li key={index}>
+                <span className="bg-blue-500 rounded p-1 m-1">{i}</span>
+              </li>
+              
+            ))}
+          </ul>
+          <hr class="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700"/>
+          </>
+          
           ))}
         </ul>
       </div>
